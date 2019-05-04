2511d1e8fe5c900e42a427834bb8305fe04f174b
Jiayu Yi <yijiayu@gmail.com>
2019-05-04 16:21:06 +0800

    Get some instructions

--- interpreter.go
+++ interpreter.go
@@ -1,5 +1,46 @@
 package main
 
+type Opcode uint8
+
+// Opcodes for standard Chip-8 instructions
+const (
+	OpCLS_00E0 Opcode = iota
+	OpRET_00EE
+	OpSYS_0nnn
+	OpJP_1nnn
+	OpCALL_2nnn
+	OpSE_3xkk
+	OpSNE_4xkk
+	OpSE_5xy0
+	OpLD_6xkk
+	OpADD_7xkk
+	OpLD_8xy0
+	OpOR_8xy1
+	OpAND_8xy2
+	OpXOR_8xy3
+	OpADD_8xy4
+	OpSUB_8xy5
+	OpSHR_8xy6
+	OpSUBN_8xy7
+	OpSHL_8xyE
+	OpSNE_9xy0
+	OpLD_Annn
+	OpJP_Bnnn
+	OpRND_Cxkk
+	OpDRW_Dxyn
+	OpSKP_Ex9E
+	OpSKNP_ExA1
+	OpLD_Fx07
+	OpLD_Fx0A
+	OpLD_Fx15
+	OpLD_Fx18
+	OpADD_Fx1E
+	OpLD_Fx29
+	OpLD_Fx33
+	OpLD_Fx55
+	OpLD_Fx65
+)
+
 // keypad is the interface for a Chip-8 keypad.
 type keypad interface {
 	// IsPressed returns true if key is currently pressed.
@@ -42,6 +83,7 @@ type Interpreter struct {
 }
 
 type instruction struct {
+	opcode Opcode
 	addr   uint16
 	nibble uint8
 	x      uint8
@@ -51,10 +93,36 @@ type instruction struct {
 
 func instrAt(data []uint8, i int) (instr instruction) {
 	hi, lo := data[i], data[i+1]
-	op := hi & 0xf0
+	op := hi & 0xf0 >> 4
 	switch op {
 	case 0:
+		switch lo {
+		case 0xE0:
+			instr.opcode = OpCLS_00E0
+		case 0xEE:
+			instr.opcode = OpRET_00EE
+		default:
+			instr.opcode = OpSYS_0nnn
+			instr.addr = uint16(hi&0x0f)<<8 + uint16(lo)
+		}
+	case 1:
+		instr.opcode = OpJP_1nnn
 		instr.addr = uint16(hi&0x0f)<<8 + uint16(lo)
+	case 2:
+		instr.opcode = OpCALL_2nnn
+		instr.addr = uint16(hi&0x0f)<<8 + uint16(lo)
+	case 3:
+		instr.opcode = OpSE_3xkk
+		instr.x = hi & 0x0f
+		instr.byte = lo
+	case 4:
+		instr.opcode = OpSNE_4xkk
+		instr.x = hi & 0x0f
+		instr.byte = lo
+	case 5:
+		instr.opcode = OpSE_5xy0
+		instr.x = hi & 0x0f
+		instr.y = lo & 0xf0 >> 4
 	}
 	return
 }
